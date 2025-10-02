97836fd59fb3653a2937d9b59b9c32918393ac5f
galih <galihmaulamumtaz@gmail.com>
2025-10-02 15:43:41 +0700

    Test Merge error modifikasi baris

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hi, ini baris pertama");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi Galih");
+        System.out.println("galih menambahkan baris baru");
     }
 }
